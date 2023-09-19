92ec5a5095db7181c3dec288d1848616cee6268b
TanveshVaity <tanveshvaity99@gmail.com>
2023-09-19 16:34:46 +0530

    fetching data from firebase

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,26 +1,73 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import classes from  "./AvailableMeals.module.css"
-import dummyMeals from "./dummyMeals";
 import MealItem from "../Meals/MealItem/MealItem";
 import Card from "../UI/Card";
+import axios from "axios";
 
 const AvailableMeals = ()=>{
-    const mealsList = dummyMeals.map((meal) => {
-        return(
-            <MealItem
-                id={meal.id}
-                key={meal.id}
-                name={meal.name}
-                description = {meal.description}
-                price= {meal.price}
-            />
-        )
-    });
+    const [meals, setMeals] = useState([]);
+    const[isLoading, setIsLoading] = useState(true);
+    const[error, setError] = useState();
 
+    useEffect(() => {
+        const fetchMeals = async () => {
+            try {
+                const response = await axios.get(
+                "https://react-meals-3e5c0-default-rtdb.firebaseio.com/meals.json"
+                );
+                if (!response.data || response.status !== 200) {
+                    throw new Error("Something went wrong!");
+                }
+                const responseData = response.data;
+        
+                const loadedMeals = Object.keys(responseData).map((key) => {
+                const { name, description, price } = responseData[key];
+                return {
+                    id: key,
+                    name,
+                    description,
+                    price,
+                };
+                });
+        
+                setMeals(loadedMeals);
+                setIsLoading(false);
+            } catch (error) {
+                console.error("Error fetching meals:", error);
+                setIsLoading(false);
+                setError(error.message);
+            }
+            };
+        
+            fetchMeals();
+        }, []);
+ 
     return(
         <section className={classes.meals}>
-            <Card >
-                <ul>{mealsList}</ul>
+            <Card>
+                {isLoading ? (
+                <section className={classes.mealsLoading}>
+                    <p>Loading....</p>
+                </section>
+                ) : error ? (
+                <section className={classes.mealsError}>
+                    <p>{error}</p>
+                </section>
+                ) : meals.length > 0 ? (
+                <ul>
+                    {meals.map((meal) => (
+                    <MealItem
+                        id={meal.id}
+                        key={meal.id}
+                        name={meal.name}
+                        description={meal.description}
+                        price={meal.price}
+                    />
+                    ))}
+                </ul>
+                ) : (
+                <p>No meals available.</p>
+                )}
             </Card>
         </section>
     )
